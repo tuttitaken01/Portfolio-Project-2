fd845f1dbfcf4d63abeae581df1824b2ca8395fa
Valentina <vh010200@gmail.com>
2023-02-07 11:23:30 +0100

    CSS styling added to homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,12 @@
 export default function Header() {
     return (
         <section className="header">
+            <div className="bg">
+                <img src="https://images.unsplash.com/photo-1514863775978-c611132e6691?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2063&q=80" alt="playing"></img>
+            </div>
             <h1 className="title">House of Games</h1>
             <p className="desc">Welcome to Gameview, a place where you can read and discuss about your favourite board games!</p>
+            
         </section>
     )
 }
