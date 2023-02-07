03bbb0c0bb159aff9d43a2e454daac4b7087be5b
Valentina <vh010200@gmail.com>
2023-02-07 17:18:28 +0100

    comments component on its own

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Header from './components/Header';
 import Nav from './components/Nav';
 import Reviews from './components/Reviews';
 import SingleRev from './components/ReviewCard';
+import Comments from './components/Comments';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 
 function App() {
@@ -15,6 +17,7 @@ function App() {
           <Route path="/" element={<Header />}></Route>
           <Route path="/reviews" element={<Reviews />}></Route>
           <Route path="/reviews/:reviewId" element={<SingleRev />}></Route>
+          <Route path="/reviews/:reviewId/comments" element={<Comments />}></Route>
         </Routes>
       </div>
     </BrowserRouter>
--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -2,36 +2,21 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { IconButton } from '@mui/material';
-import { ArrowCircleDown, ArrowCircleUp, ThumbDown, ThumbUp } from '@mui/icons-material';
+import { ThumbDown, ThumbUp } from '@mui/icons-material';
 
 export default function SingleRev() {
     const [ review, setReview ] = useState({});
-    const [ comments, setComments ] = useState([]);
     const [ loading, setLoading ] = useState(false);
-    const [ msg, setMsg ] = useState(null);
     const { reviewId } = useParams();
 
     useEffect(() => {
         const fetchReview = async () => {
             setLoading(true);
             try {
-                const [ revRes, comRes ] = await Promise.all([
-                    axios.get(`https://gameview.onrender.com/api/reviews/${reviewId}`),
-                    axios.get(`https://gameview.onrender.com/api/reviews/${reviewId}/comments`),
-                ]);
-                // console.log(response.data.review);
+                const revRes = await axios.get(`https://gameview.onrender.com/api/reviews/${reviewId}`);
                 let rev = revRes.data.review;
-                let com = comRes.data.comments;
-                // console.log(com);
                 rev.created_at = (new Date(rev.created_at)).toUTCString();
-                // console.log(typeof(result.created_at))
                 setReview(rev);
-                setComments(com);
-                if (rev.comment_count === "0") {
-                    setMsg(`It's very dry here...🏜️`);
-                } else {
-
-                }
             } finally {
                 setLoading(false);
             }
@@ -54,20 +39,6 @@ export default function SingleRev() {
                 <ThumbDown fontSize='small' color='error' />
                 </IconButton>
             </p> 
-            <h3>Comments ({review.comment_count})</h3>
-            {msg && <p>{msg}</p>}
-            <ul className='comments'>
-                {comments.map(c => (
-                    <li key={c.comment_id} className="comitem">
-                        <h4 className="author">{c.author}</h4>
-                        {c.body}
-                    <p className="votes"><IconButton aria-label='up'>
-                    <ArrowCircleUp fontSize='small' color='success' />
-                    </IconButton>{c.votes}<IconButton aria-label="down">
-                    <ArrowCircleDown fontSize='small' color='error' />
-                    </IconButton></p></li>
-                ))}
-            </ul>
         </div>
         
     )
